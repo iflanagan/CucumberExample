406a4bae1b984a802beb9f04c61b71f9bb5d6445
Ian Flanagan <ianianf@yahoo.comi>
2023-01-11 13:36:23 -0600

    Removed token and projectID from MyConfiguration.java

--- src/test/java/Steps/MyConfiguration.java
+++ src/test/java/Steps/MyConfiguration.java
@@ -2,7 +2,7 @@ package Steps;
 
 public class MyConfiguration {
 
-    public static final String token = "GqKVj97IUUOXDrAJPirAty6HsMIGUZMqvRi4mm7IlvlBupA0tf";
-    public static final String project = "iLyLKuuWLZ2UaItwcRDP";
-    public static final String myConfigFile = "/Users/ianflanagan/testimConfig.js";
+    public static final String token = "<yourToken>";
+    public static final String project = "<yourProject>";
+    public static final String myConfigFile = "/<yourhomedir>/testimConfig.js";
 }
